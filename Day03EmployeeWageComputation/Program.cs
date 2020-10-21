9df19f3385651b12bdc1442636bc675101dce583
Ramya.U <ramyayadav2016@gmail.com>
2020-10-21 19:40:23 +0530

    [Ramya] Refactor . Modified Program.cs

--- Day03EmployeeWageComputation/Program.cs
+++ Day03EmployeeWageComputation/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/******************************************************************************
+ *  Compilation:  csc.exe /t:exe /out:Day03EmployeeWageCompuatation.exe Day03EmployeeWageCompuatation.cs
+ *  Execution:    Day03EmployeeWageCompuatation.exe 103
+ *  
+ *  Purpose: Determines whether employee is present or not
+ *
+ *  @author  Ramya U
+ *  @version 1.0
+ *  @since   21-10-2020
+ *
+ ******************************************************************************/
+using System;
 
 namespace Day03EmployeeWageComputation
 {
@@ -6,7 +17,22 @@ namespace Day03EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Wage Computation!");
+            //constants
+            const int IS_FULL_TIME = 1;
+            //reference for random class
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
+
